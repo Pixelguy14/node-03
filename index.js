a3b66870e661c41b59d84b0f9b0ff04905984f9e
José Julián Sierra Álvarez <160289138+Pixelguy14@users.noreply.github.com>
2024-08-26 19:58:19 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 import estudiantesRoutes from './routes/estudiantes.js'
 
 //Initialize server
 const app = express()
+const corsOptions = {
+    origin: '*',
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions))
 app.use(bodyParser.json())
 
-
-//Route management
 app.use('/api/estudiantes',estudiantesRoutes)
 
-const PORT = process.env.PORT || 3010
+const PORT = process.env.PORT || 4010
 
 app.listen(PORT, () => {
     console.log(`Escuchando el puerto: ${PORT}`)
